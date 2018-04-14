af9fd00e4759589721b7586511221b7ebe6d57a1
Daniel <redbanana10@yahoo.com>
2018-04-14 13:57:28 +1200

    Added bluetooth stuff

--- app/src/main/java/com/bike/team/compx241_xercisebike/MainActivity.java
+++ app/src/main/java/com/bike/team/compx241_xercisebike/MainActivity.java
@@ -1,13 +1,28 @@
 package com.bike.team.compx241_xercisebike;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int REQUEST_ENABLE_BT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        } else {
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
     }
 }
